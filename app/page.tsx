7ec2ac1904a6ee210077acf9e72c9a69e30bc4bf
Hardtech <hardtech@Hardtechs-MacBook-Pro.local>
2023-11-01 19:45:01 +0300

    here we go

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <div className="text-center font-semibold text-blue-900 text-3xl">
+        Create amazing letters, with one, two, three steps 😄
+      </div>
+    </main>
+  )
+}
+
+export default Page
