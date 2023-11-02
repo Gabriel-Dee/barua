0a2956081313c58b4adfbc2bcd34b24f1b63285f
Hardtech <hardtech@Hardtechs-MacBook-Pro.local>
2023-11-02 13:55:00 +0300

    button and pages and prettier config

---
+++ app/auth/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Barua',
+  description: 'Creative way of writing letters of all kinds',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/components/button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button() {
+  return (
+    <main className="flex flex-row px-[32px] py-[12px] bg-slate-700 rounded-xl  justify-center items-center">
+      <section className="text-slate-100 font-medium text-[24px]">Here we go 🔥</section>
+    </main>
+  );
+}
+
+export default Button;
