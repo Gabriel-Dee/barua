e288953e3ee521748f1c63336d97aeb9a5c20b7f
Gabriel D Minzemalulu <90185518+Gabriel-Dee@users.noreply.github.com>
2023-11-02 17:06:03 +0300

    Create NavBar.tsx

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,103 @@
+'use client'
+
+import { useState } from 'react';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="bg-white shadow dark:bg-gray">
+      <div className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
+        <div className="flex items-center justify-between">
+          <a href="#">
+            <img
+              className="w-auto h-6 sm:h-7"
+              src="https://merakiui.com/images/full-logo.svg"
+              alt=""
+            />
+          </a>
+          <div className="flex lg:hidden">
+            <button
+              onClick={toggleMenu}
+              type="button"
+              className="text-gray-900 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+              aria-label="toggle menu"
+            >
+              <svg
+                style={{ display: isOpen ? 'none' : 'block' }}
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
+              </svg>
+              <svg
+                style={{ display: isOpen ? 'block' : 'none' }}
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        {/* Mobile Menu open: "block", Menu closed: "hidden" */}
+        <div
+          className={`${
+            isOpen ? 'block' : 'hidden'
+          } md:flex absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto`}
+        >
+          <div className="flex flex-col md:flex-row items-center md:items-start">
+            <a
+              className="my-2 text-black transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+              href="#"
+            >
+              Home
+            </a>
+            <a
+              className="my-2 text-black transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+              href="#"
+            >
+              Shop
+            </a>
+            <a
+              className="my-2 text-black transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+              href="#"
+            >
+              Contact
+            </a>
+            <a
+              className="my-2 text-black transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+              href="#"
+            >
+              About
+            </a>
+          </div>
+          <div className="md:flex">
+            <a
+              className="my-2 text-black transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+              href="#"
+            >
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
