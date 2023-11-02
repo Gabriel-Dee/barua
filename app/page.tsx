0e6e34837e8602747982f5d7b60cb1d27c97ef0c
Gabriel D Minzemalulu <90185518+Gabriel-Dee@users.noreply.github.com>
2023-11-02 14:30:56 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Button from './components/button'
+import Services from './components/services'
 
 function Page() {
   return (
-    <main className="flex flex-col gap-8 items-center justify-center h-screen">
-      <div className="text-center font-semibold text-blue-900 text-3xl">
-        Create amazing letters, with one, two, three steps 😄
-      </div>
-      <Button/>
-    </main>
+    <>
+      <main className="flex flex-col gap-8 items-center justify-center h-screen">
+        <div className="text-center font-semibold text-blue-900 text-3xl">
+          Create amazing letters, with one, two, three steps 😄
+        </div>
+        <Button />
+      </main>
+      <Services />
+    </>
+
   )
 }
 
