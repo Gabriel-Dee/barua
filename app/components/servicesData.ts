17a4b2ae0567f7013702691332a464950e7716e5
Gabriel D Minzemalulu <90185518+Gabriel-Dee@users.noreply.github.com>
2023-11-03 10:22:49 +0300

    Update servicesData.ts

--- app/components/servicesData.ts
+++ app/components/servicesData.ts
@@ -14,20 +14,5 @@ export const services = [
       icon: 'bi-badge-ad',
       description: 'This is a wider card with supporting text below as a natural content.',
     },
-    {
-      title: 'Brand Identity',
-      icon: 'bi-card-checklist',
-      description: 'This is a wider card with supporting text below as a natural content.',
-    },
-    {
-      title: 'Budget & Marketing',
-      icon: 'bi-wallet2',
-      description: 'This is a wider card with supporting text below as a natural content.',
-    },
-    {
-      title: 'Optimize conversions',
-      icon: 'bi-funnel',
-      description: 'This is a wider card with supporting text below as a natural content.',
-    },
   ];
   
