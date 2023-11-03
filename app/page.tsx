03e54a033cb60c261e835a8b9e4a59ed84dd6699
Gabriel D Minzemalulu <90185518+Gabriel-Dee@users.noreply.github.com>
2023-11-03 11:42:42 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,13 @@ import Services from './components/services'
 import Navbar from './components/Navbar'
 import CTA from './components/CTA'
 import Footer from './components/Footer'
+import Hero from './components/Hero'
 
 function Page() {
   return (
     <>
       <Navbar />
-      <main className="flex flex-col gap-8 items-center justify-center h-screen">
-        <div className="text-center font-semibold text-blue-900 text-3xl">
-          Create amazing letters, with one, two, three steps 😄
-        </div>
-        <Button />
-      </main>
+      <Hero />
       <CTA />
       <Services />
       <Footer />
